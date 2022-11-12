b7649e1f737a961bab83a22e9cacc9c407fe1d00
Nicolas Barbarisi <nico.bar2012@gmail.com>
2022-11-12 14:34:35 +0100

    #9 : Add eslint

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,7 +1,7 @@
-var elem = document.createElement('div');
-elem.setAttribute("id", "main");
-document.body.appendChild(elem);
+const elem = document.createElement('div')
+elem.setAttribute('id', 'main')
+document.body.appendChild(elem)
 
-var heading = document.createElement('h1');
-heading.innerHTML = "Hello World!";
-elem.appendChild(heading);
+const heading = document.createElement('h1')
+heading.innerHTML = 'Hello World!'
+elem.appendChild(heading)
